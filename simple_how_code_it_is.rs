8072a35e908dbf40d48583d687e93d10091afc88
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2022-12-19 00:59:03 +0530

    Create simple_how_code_it_is.rs

---
+++ simple_how_code_it_is.rs
@@ -0,0 +1,28 @@
+let gb : GitBotInit;
+ 
+fn do_initialize_git()->
+{
+ gb.GitInit("");
+}
+
+fn do_add_files(file : string)->
+{
+ gb.GitAddFile(file);
+ }
+ 
+ fn add_remote_origin(username : string, respository_name : string)->
+ {
+    let url_main = "https://github.com/"+username+"/"+respository_name+".git";
+    gb.GitRemoteToOrigin(url_main);
+ }
+ 
+ fn upload_all_files()->
+ {
+    gb.GitUploadFiles(); // git push origin ..
+ }
+ 
+ fn download_files()->
+ {
+   gb.GitDownloadFiles(); // git pull origin ..
+ }
+    
